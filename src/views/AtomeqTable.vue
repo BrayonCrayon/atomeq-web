da6604cfd41c0f623803abde82e463895f856a28
e.stroganova <e.stroganova@vehikl.com>
2025-09-08 17:01:01 -0400

    Introduce Periodic Table - Introduce axios WIP - fetch element data from backend API - CORS error
    Co-authored-by: CK <c.teoh@vehikl.com>

--- src/views/AtomeqTable.vue
+++ src/views/AtomeqTable.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
+import axios from 'axios';
 
+const url = "http://localhost/elements";
+
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+console.log(data.value);
 </script>
 
 <template>
