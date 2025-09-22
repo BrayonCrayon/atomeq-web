97399f54c6548435335f9000d982d05eb7055d69
Brady Deroy <b.deroy@vehikl.com>
2025-09-22 16:34:31 -0400

    - Generate the periodic table - Fix tests
    Co-authored-by: Ekaterina Stroganova <e.stroganova@vehikl.com>

--- src/views/AtomeqTable.vue
+++ src/views/AtomeqTable.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import useElements from '@/composables/useElements.ts';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+import { getElementTable } from '@/helpers/tableUtils.ts';
 
 const { getElements, elements } = useElements();
+
+const elementTable = computed(() => {
+  return getElementTable(elements.value);
+});
 
 onMounted(async () => {
   await getElements();
@@ -12,9 +17,9 @@ onMounted(async () => {
 <template>
   <div class="p-4">
     <h1 class="text-2xl">Periodic Table:</h1>
-    <div class="grid grid-cols-18 gap-2">
-      <div :key="element.id" v-for="element in elements" class="border-2 rounded h-20 shadow-md">
-        {{ element.symbol }}
+    <div :key="idx" v-for="(row, idx) in elementTable" class="grid grid-cols-18 gap-2">
+      <div :key="idx2" v-for="(element, idx2) in row" class="border-2 rounded h-20 shadow-md">
+        {{ element?.symbol ?? 'none' }}
       </div>
     </div>
   </div>
