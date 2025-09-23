be0580c7dfc2b828e7c1de88770fa983121e1238
e.stroganova <e.stroganova@vehikl.com>
2025-09-23 16:55:06 -0400

    - WIP - radioactive group table
    Co-authored-by: Brady Deroy <b.deroy@vehikl.com>

--- src/helpers/tableUtils.ts
+++ src/helpers/tableUtils.ts
@@ -1,4 +1,5 @@
 import type { AtomeqElement } from '@/types/element.ts';
+import { groupBy } from 'lodash';
 
 export const getElementTable = (
   elements: AtomeqElement[] = [],
@@ -14,4 +15,39 @@ export const getElementTable = (
   return base;
 };
 
-export default { getElementTable };
+export const getRadioactiveElementTable = (
+  elements: AtomeqElement[] = [],
+): (AtomeqElement | undefined)[][] => {
+  const base: (AtomeqElement | undefined)[][] = Array.from({ length: 2 }).map(() =>
+    Array.from({ length: 14 }).map(() => undefined),
+  );
+
+  console.log(
+    'elements',
+    elements.map((item) => ({
+      group: item.group,
+      period: item.period,
+      atomicNum: item.atomicNumber,
+    })),
+  );
+
+  const bottomRow = groupBy(elements, 'period');
+
+  // group from 3 to 16
+  // period 6 and 7
+  // is data inconsistent? LA has group, CE has group 0
+  // the data is shifted, LR (#103) took place of LA(#57)
+
+  // WIP - are they in order?
+  console.log('in a base loop, bottom row', bottomRow);
+
+  for (const element of elements) {
+    let index = 3;
+    base[element.period - 1][index] = element;
+    index++;
+  }
+
+  return base;
+};
+
+export default { getElementTable, getRadioactiveElementTable };
--- src/views/AtomeqTable.vue
+++ src/views/AtomeqTable.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import useElements from '@/composables/useElements.ts';
 import { computed, onMounted } from 'vue';
-import { getElementTable } from '@/helpers/tableUtils.ts';
+import { getElementTable, getRadioactiveElementTable } from '@/helpers/tableUtils.ts';
 
 const { getElements, elements } = useElements();
 
 const elementTable = computed(() => {
   return getElementTable(elements.value);
+});
+
+const radioactiveGroup = computed(() => elements.value.filter((element) => element.group === 0));
+
+const radioactiveGroupTable = computed(() => {
+  console.log(radioactiveGroup.value);
+  return getRadioactiveElementTable(radioactiveGroup.value);
 });
 
 onMounted(async () => {
@@ -19,7 +26,12 @@ onMounted(async () => {
     <h1 class="text-2xl">Periodic Table:</h1>
     <div :key="idx" v-for="(row, idx) in elementTable" class="grid grid-cols-18 gap-2">
       <div :key="idx2" v-for="(element, idx2) in row" class="border-2 rounded h-20 shadow-md">
-        {{ element?.symbol ?? 'none' }}
+        {{ element?.symbol ?? ' ' }}
+      </div>
+    </div>
+    <div :key="idx" v-for="(row, idx) in radioactiveGroupTable" class="grid grid-cols-14 gap-2">
+      <div :key="idx2" v-for="(element, idx2) in row" class="border-2 rounded h-20 shadow-md">
+        {{ element?.symbol ?? ' ' }}
       </div>
     </div>
   </div>
