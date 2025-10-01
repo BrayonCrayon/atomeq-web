a65d5aee212d75fd3c8a761e1ade87c945782950
e.stroganova <e.stroganova@vehikl.com>
2025-10-01 16:26:48 -0400

    Fix up failing tests
    Co-authored-by: Nick Whitford <n.whitford@vehikl.com>

--- src/types/element.ts
+++ src/types/element.ts
@@ -71,7 +71,7 @@ export class AtomeqElement implements IAtomeqElement {
     this.electronegativity = overrides.electronegativity ?? 0;
     this.electrons = overrides.electrons ?? 0;
     this.elementStateId = overrides.elementStateId ?? 0;
-    this.elementState = new AtomeqElementState() ?? undefined;
+    this.elementState = new AtomeqElementState(overrides.elementState) ?? undefined;
     this.firstIonization = overrides.firstIonization ?? 0;
     this.group = overrides.group ?? 0;
     this.id = overrides.id ?? 0;
@@ -89,7 +89,7 @@ export class AtomeqElement implements IAtomeqElement {
     this.specificHeat = overrides.specificHeat ?? 0;
     this.symbol = overrides.symbol ?? '';
     this.typeId = overrides.typeId ?? 0;
-    this.type = new AtomeqElementType() ?? undefined;
+    this.type = new AtomeqElementType(overrides.type) ?? undefined;
     this.valence = overrides.valence ?? 0;
   }
 }
