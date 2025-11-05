de795470c098e9881050600bb21aa6265f4e1fc9
Brady Deroy <b.deroy@vehikl.com>
2025-11-05 16:51:59 -0500

    Element Modal - Partial styling - Display keys
    AtomeqBadge
    - Create component
    
    Create custom colour
    
    Co-authored-by: Ian <i.ewing@vehikl.com>

--- src/components/AtomeqElementDetails.vue
+++ src/components/AtomeqElementDetails.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { AtomeqElement } from '@/types/element.ts';
+import AtomeqBadge from '@/components/common/AtomeqBadge.vue';
+import { Variant } from '@/types/common.ts';
 
 defineProps<{
   element: AtomeqElement;
@@ -7,29 +9,38 @@ defineProps<{
 </script>
 
 <template>
-  <div>{{ element.name }}</div>
-  <div>{{ element.symbol }}</div>
-  <div>{{ element.atomicNumber }}</div>
-  <div>{{ element.atomicMass }}</div>
-  <div>{{ element.atomicRadius }}</div>
-  <div>{{ element.boilingPoint }}</div>
-  <div>{{ element.density }}</div>
-  <div>{{ element.electronegativity }}</div>
-  <div>{{ element.electrons }}</div>
-  <div>{{ element.firstIonization }}</div>
-  <div>{{ element.group }}</div>
-  <div>{{ element.isotopes }}</div>
-  <div>{{ element.meltingPoint }}</div>
-  <div>{{ element.metal }}</div>
-  <div>{{ element.metalloid }}</div>
-  <div>{{ element.natural }}</div>
-  <div>{{ element.neutrons }}</div>
-  <div>{{ element.period }}</div>
-  <div>{{ element.protons }}</div>
-  <div>{{ element.radioactive }}</div>
-  <div>{{ element.shells }}</div>
-  <div>{{ element.specificHeat }}</div>
-  <div>{{ element.valence }}</div>
+  <div class="grid grid-cols-4 bg-gray-200 rounded-lg p-2 capitalize">
+    <div>{{ element.name }}</div>
+    <div>symbol: {{ element.symbol }}</div>
+    <div>atomicNumber: {{ element.atomicNumber }}</div>
+    <div>atomicMass: {{ element.atomicMass }}</div>
+    <div>atomicRadius: {{ element.atomicRadius }}</div>
+    <div>boilingPoint: {{ element.boilingPoint }}</div>
+    <div>density: {{ element.density }}</div>
+    <div>electronegativity: {{ element.electronegativity }}</div>
+    <div>electrons: {{ element.electrons }}</div>
+    <div>firstIonization: {{ element.firstIonization }}</div>
+    <div>group: {{ element.group }}</div>
+    <div>isotopes: {{ element.isotopes }}</div>
+    <div>meltingPoint: {{ element.meltingPoint }}</div>
+    <div>metal: {{ element.metal }}</div>
+    <div>metalloid: {{ element.metalloid }}</div>
+
+    <div class="flex gap-2 rounded-full px-4 py-0.5 items-center">
+      <AtomeqBadge
+        text="Naturally Occurring"
+        :variant="element.natural ? Variant.SUCCESS : Variant.DANGER"
+      />
+    </div>
+
+    <div>neutrons: {{ element.neutrons }}</div>
+    <div>period: {{ element.period }}</div>
+    <div>protons: {{ element.protons }}</div>
+    <div>radioactive: {{ element.radioactive }}</div>
+    <div>shells: {{ element.shells }}</div>
+    <div>specificHeat: {{ element.specificHeat }}</div>
+    <div>valence: {{ element.valence }}</div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/common/AtomeqBadge.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Ban, Check } from 'lucide-vue-next';
+import { Variant } from '@/types/common.ts';
+
+defineProps<{
+  text: string;
+  variant?: Variant;
+}>();
+</script>
+
+<template>
+  <div
+    class="flex gap-2 rounded-full px-4 py-0.5 items-center"
+    :class="{
+      'bg-error-badge-500': variant === Variant.DANGER,
+      'bg-green-500': variant === Variant.SUCCESS,
+    }"
+  >
+    <p>{{ text }}</p>
+    <Check v-if="variant === Variant.SUCCESS" :size="18" />
+    <Ban v-else :size="18" />
+  </div>
+</template>
+
+<style scoped></style>
