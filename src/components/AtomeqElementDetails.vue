56a6830a1bfb73a7e9ea05163a28c45aeda53b9c
Brady Deroy <b.deroy@vehikl.com>
2025-11-12 16:57:38 -0500

    Style up Modal to display element info
    Co-authored-by: Ekaterina Stroganova <e.stroganova@vehikl.com>

--- src/components/AtomeqElementDetails.vue
+++ src/components/AtomeqElementDetails.vue
@@ -9,37 +9,61 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-4 bg-gray-200 rounded-lg p-2 capitalize">
-    <div>{{ element.name }}</div>
-    <div>symbol: {{ element.symbol }}</div>
-    <div>atomicNumber: {{ element.atomicNumber }}</div>
-    <div>atomicMass: {{ element.atomicMass }}</div>
-    <div>atomicRadius: {{ element.atomicRadius }}</div>
-    <div>boilingPoint: {{ element.boilingPoint }}</div>
-    <div>density: {{ element.density }}</div>
-    <div>electronegativity: {{ element.electronegativity }}</div>
-    <div>electrons: {{ element.electrons }}</div>
-    <div>firstIonization: {{ element.firstIonization }}</div>
-    <div>group: {{ element.group }}</div>
-    <div>isotopes: {{ element.isotopes }}</div>
-    <div>meltingPoint: {{ element.meltingPoint }}</div>
-    <div>metal: {{ element.metal }}</div>
-    <div>metalloid: {{ element.metalloid }}</div>
+  <div class="bg-gray-200 rounded-lg p-2 capitalize w-[620px] flex justify-between">
+    <div class="">
+      <div class="border-2 p-2 rounded-lg h-38 w-38 mb-2" :class="element.typeColour">
+        <p class="font-semibold">{{ element.atomicNumber }}</p>
+        <div class="text-5xl font-bold text-gray-900">
+          {{ element.symbol }}
+        </div>
+        <div class="text-2xl text-gray-900 font-semibold">
+          {{ element.name }}
+        </div>
+        <div class="text-gray-900 font-semibold">({{ element.atomicMass }})</div>
+      </div>
 
-    <div class="flex gap-2 rounded-full px-4 py-0.5 items-center">
-      <AtomeqBadge
-        text="Naturally Occurring"
-        :variant="element.natural ? Variant.SUCCESS : Variant.DANGER"
-      />
+      <div>
+        <div class="flex gap-2 rounded-full py-0.5 items-center">
+          <AtomeqBadge text="Metal" :variant="element.metal ? Variant.SUCCESS : Variant.DANGER" />
+        </div>
+        <div class="flex gap-2 rounded-full py-0.5 items-center">
+          <AtomeqBadge
+            text="Metalloid"
+            :variant="element.metalloid ? Variant.SUCCESS : Variant.DANGER"
+          />
+        </div>
+        <div class="flex gap-2 rounded-full py-0.5 items-center">
+          <AtomeqBadge
+            text="Naturally Occurring"
+            :variant="element.natural ? Variant.SUCCESS : Variant.DANGER"
+          />
+        </div>
+        <div class="flex gap-2 rounded-full py-0.5 items-center">
+          <AtomeqBadge
+            text="Radioactive"
+            :variant="element.radioactive ? Variant.SUCCESS : Variant.DANGER"
+          />
+        </div>
+      </div>
     </div>
 
-    <div>neutrons: {{ element.neutrons }}</div>
-    <div>period: {{ element.period }}</div>
-    <div>protons: {{ element.protons }}</div>
-    <div>radioactive: {{ element.radioactive }}</div>
-    <div>shells: {{ element.shells }}</div>
-    <div>specificHeat: {{ element.specificHeat }}</div>
-    <div>valence: {{ element.valence }}</div>
+    <div class="grid grid-cols-2">
+      <div>atomicRadius: {{ element.atomicRadius }}</div>
+      <div>group: {{ element.group }}</div>
+      <div>period: {{ element.period }}</div>
+      <div>electrons: {{ element.electrons }}</div>
+      <div>isotopes: {{ element.isotopes }}</div>
+      <div>neutrons: {{ element.neutrons }}</div>
+      <div>protons: {{ element.protons }}</div>
+      <div>boilingPoint: {{ element.boilingPoint }}</div>
+      <div>meltingPoint: {{ element.meltingPoint }}</div>
+      <div>density: {{ element.density }}</div>
+      <div>electronegativity: {{ element.electronegativity }}</div>
+      <div>firstIonization: {{ element.firstIonization }}</div>
+      <div>shells: {{ element.shells }}</div>
+      <div>specificHeat: {{ element.specificHeat }}</div>
+      <div>valence: {{ element.valence }}</div>
+    </div>
   </div>
 </template>
 
