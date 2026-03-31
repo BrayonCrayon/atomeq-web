75daf37827502ab3a1e28f6f28a4fc825894a4f7
e.stroganova <e.stroganova@vehikl.com>
2026-03-31 16:28:45 -0400

    Ensure AtomeqStateLegend emits all necessary events
    Co-authored-by: Brady Deroy <b.deroy@vehikl.com>

--- src/components/__tests__/AtomeqStateLegend.spec.ts
+++ src/components/__tests__/AtomeqStateLegend.spec.ts
@@ -1,6 +1,6 @@
 import AtomeqStateLegend from '@/components/AtomeqStateLegend.vue';
-import type { ElementState } from '@/types/elementState.ts';
-import { apiService } from '@/vitest.setup.ts';
+import { elementStateFactory } from '@/testUtils/elementStateFactory.ts';
+import { apiService, generateAxiosResponse } from '@/vitest.setup.ts';
 import { flushPromises, mount } from '@vue/test-utils';
 import type { AxiosResponse } from 'axios';
 import { describe, it, expect } from 'vitest';
@@ -15,37 +15,28 @@ const mountComponent = () => {
 
 describe('AtomeqStateLegend', () => {
   it('calls the api to retrieve all data on element state', () => {
-    const response = { data: [] };
-    apiService.fetchStates.mockResolvedValue(response as AxiosResponse);
+    apiService.fetchStates.mockResolvedValue(generateAxiosResponse([]));
     mountComponent();
 
     expect(apiService.fetchStates).toHaveBeenCalled();
   });
 
-  // it.each([
-  //   ['mouseover', 'hover'],
-  //   ['mouseleave', 'hoverLeave'],
-  //   ['click', 'click'],
-  // ])('emits an event when an element %s is triggered', async (eventTrigger, expectedEvent) => {
-  //   const bundle = setupTest();
-  //   const [parent, child] = bundle;
-  //
-  //   const response = { data: bundle };
-  //   apiService.fetchTypes.mockResolvedValue(response as AxiosResponse);
-  //
-  //   const wrapper = mountComponent();
-  //
-  //   await flushPromises();
-  //
-  //   const parentElement = wrapper.find(`#parent-${parent.id}`);
-  //   await parentElement.trigger(eventTrigger);
-  //
-  //   const childElement = wrapper.find(`#child-${child.id}`);
-  //   await childElement.trigger(eventTrigger);
-  //
-  //   expect(wrapper.emitted(expectedEvent)).toBeDefined();
-  //   expect(wrapper.emitted(expectedEvent)).toHaveLength(2);
-  //   expect(wrapper.emitted(expectedEvent)![0]).toContainEqual(parent);
-  //   expect(wrapper.emitted(expectedEvent)![1]).toContainEqual(child);
-  // });
+  it.each([
+    ['mouseover', 'hover'],
+    ['mouseleave', 'hoverLeave'],
+    ['click', 'click'],
+  ])('emits an event when an element %s is triggered', async (eventTrigger, expectedEvent) => {
+    const state = elementStateFactory();
+    apiService.fetchStates.mockResolvedValue(generateAxiosResponse([state]));
+
+    const wrapper = mountComponent();
+
+    await flushPromises();
+
+    const stateElement = wrapper.find(`#state-${state.id}`);
+    await stateElement.trigger(eventTrigger);
+
+    expect(wrapper.emitted(expectedEvent)).toBeDefined();
+    expect(wrapper.emitted(expectedEvent)![0]).toContainEqual(state);
+  });
 });
